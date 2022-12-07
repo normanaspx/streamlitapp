eb8924cfb6adbe61d7b2663997351ca1d6628490
Norman Vicente <norman.vicente@galileo.edu>
2022-12-06 23:54:31 -0600

    Update modelo_prod.py

--- modelo_prod.py
+++ modelo_prod.py
@@ -17,7 +17,7 @@ face_model = cv2.CascadeClassifier(cv2.data.haarcascades
 mask_label = {0: 'MASK', 1: 'NO MASK'}
 dist_label = {0: (0, 255, 0), 1: (255, 0, 0)}  # rectangle color
 
-model = load_model('modelo.h5')
+model = null
 def plot_image(image, subplot):
     plt.subplot(*subplot)
     plt.imshow(image)
